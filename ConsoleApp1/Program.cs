f20210631b30b8497ec48e7627a44a67b91eb13e
Frederico <fredericoboller@outlook.com>
2025-03-19 18:05:05 -0300

    03/19 18:03 - Pegando valor do ativo, monitora a cada 30s

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Dynamic;
 using System.IO;
 using System.Net.Http;
 using System.Net.Http.Json;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 // See https://aka.ms/new-console-template for more information
@@ -9,17 +12,17 @@ using System.Threading.Tasks;
 
 public class Call {
     private static string token = "gVmotaaAMJLzifhZ1dozeM";
-    private static string baseAddress = "https://brapi.dev/api/quote/PETR4";
+    private static string baseAddress = "https://brapi.dev/api/quote";
     private static HttpClient client = new(){};
 
-    public static async Task GetAsync() {
+    public static async Task<HttpResponseMessage?> GetAsync(string ticker) {
         var parameters = new Dictionary<string, string>
         {
             { "token", token }
         };
 
         var query = new FormUrlEncodedContent(parameters).ReadAsStringAsync().Result;
-        string requestUrl = $"{baseAddress}?{query}";
+        string requestUrl = $"{baseAddress}/{ticker}?{query}";
 
         try {
             HttpResponseMessage response = await client.GetAsync(requestUrl);
@@ -33,16 +36,50 @@ public class Call {
             {
                 Console.WriteLine($"Request failed with status code {response.StatusCode}");
             }
+            return response;
         }
         catch (Exception ex)
         {
             Console.WriteLine($"Error: {ex.Message}");
+            return null;
         }
     }
 } 
 
+public class APIResponse {
+    public List<APIResults>? results { get; set; }
+}
+
+public class APIResults {
+    public double regularMarketPrice { get; set; }
+}
 public class MainProgram {
+    private static double lowerBound = 36.03, upperBound = 36.40;
+
     public static async Task Main() {
-        await Call.GetAsync();
+
+        while(true){
+            HttpResponseMessage? response = await Call.GetAsync("PETR4");
+
+            if(response is not null){
+                string responseData = await response.Content.ReadAsStringAsync();
+
+                double currentPrice = JsonSerializer.Deserialize<APIResponse>(responseData)!.results![0].regularMarketPrice;
+
+                Console.WriteLine(currentPrice);
+
+                if(currentPrice < lowerBound) {
+                    Console.WriteLine("\nPreço de compra.\n");
+                }
+                else if(currentPrice > upperBound) {
+                    Console.WriteLine("\nPreço de venda.\n");
+                }
+            }
+
+            Console.WriteLine("Oi");
+
+            await Task.Delay(TimeSpan.FromSeconds(10));
+        }
+
     }
 }
